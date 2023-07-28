fdaefd90d172d13460023c8030cb8bed9630e607
Squirrelliz <96637363+Squirrelliz@users.noreply.github.com>
2023-07-29 00:30:51 +0300

    Update App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,11 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 import './App.css';
 import Test from "./Test";
-import {QuestionBox} from "./QuestionBox";
-import {GetQuestionBoxContent} from "./QuestionBox";
-
 import TestFooter from "./Footer";
 function App() {
     return (
         <>
             <Test name={"First Test"} questions={[]}/>
-            <QuestionBox content={GetQuestionBoxContent(2, "Самый крутой вопрос в мире от самое длинной письки в мире", ["my dick is big", "oh, myy", "afdsasd"], 0, 2, '3123')}/>
-            <QuestionBox content={GetQuestionBoxContent(1, "Самый крутой вопрос в мире от самое длинной письки в мире", ["my dick is big", "oh, myy"], 1, 1, 'йцуйцу')}/>
-            <QuestionBox content={GetQuestionBoxContent(0, "Самый крутой вопрос в мире от самое длинной письки в мире", ["my dick is big", "oh, myy"], 2, 5, 'йуцйцуй')}/>
             <TestFooter></TestFooter>
         </>
     );
