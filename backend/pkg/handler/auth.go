7027a575cfcac65ed38324b876f9175ab6979657
Vladislav <vlad.vavilin73@gmail.com>
2023-07-30 23:57:22 +0300

    начинаем делать авторизацию и аунтефикацию (JWT)

--- backend/pkg/handler/auth.go
+++ backend/pkg/handler/auth.go
@@ -1,1 +1,36 @@
 package handler
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const HeaderAuthKey = "Authorization"
+
+const UserIDContextKey = "UserID"
+
+type TokenValidator interface {
+	GetUserIDFromToken(ctx context.Context, token string) (string, error)
+}
+
+func GinAuthMiddleware(validator TokenValidator) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.Header.Get(HeaderAuthKey)
+
+		if len(token) == 0 {
+			c.AbortWithError(http.StatusUnauthorized, errors.New("Unauthorized"))
+		}
+
+		id, err := validator.GetUserIDFromToken(c.Request.Context(), token)
+
+		if err != nil {
+			c.AbortWithError(http.StatusUnauthorized, errors.New("Unauthorized"))
+		}
+
+		// TODO: replace
+		c.AddParam(UserIDContextKey, id)
+	}
+}
